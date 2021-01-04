d6d653592077572f05690398f091fe4addf4e6c9
Youssef Sabry <youssef.sabry@hotmail.com>
2021-01-04 14:56:39 -0500

    secure now

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-Author: Youssef Sabry
 
-Email: youssefsabry@cmail.carleton.ca
+Link to hosted site: https://Yousabry.github.io/movie-library
 
 
 Info / features:
 
     - shortlist is saved in cookies so refresh the page & the list will still be there
-    
+
     - search returns top 10 matches, click load more button on the bottom to see more matches
-    
+
     - click the eye icon in the table to see the poster for the movie
     
     - click the garbage can next to the item in the shortlist to remove it
--- src/components/ShortlistComponent.tsx
+++ src/components/ShortlistComponent.tsx
@@ -8,59 +8,59 @@ import { DeleteOutlined } from "@ant-design/icons";
 import "./ShortlistComponent.css";
 
 export class ShortlistComponent extends React.Component<
-  IShortlistComponentProps,
-  IShortlistComponentState
+	IShortlistComponentProps,
+	IShortlistComponentState
 > {
-  render() {
-    const numMovies: number = this.props.shortList.length;
+	render() {
+		const numMovies: number = this.props.shortList.length;
 
-    return (
-      <div className={"shortlist-container"}>
-        <h2 style={{ width: "100%", textAlign: "center" }}>Your nominations</h2>
-        <p style={{ width: "100%", textAlign: "center", fontSize: "0.9em" }}>
-          (select up to 5 movies to nominate)
-        </p>
-        <List
-          itemLayout="horizontal"
-          bordered
-          dataSource={this.props.shortList}
-          renderItem={(movie: IMovie) => (
-            <List.Item
-              actions={[
-                <DeleteOutlined
-                  color={"#eb2f96"}
-                  onClick={() => this.props.onRemoveMovie(movie.imdbID)}
-                />,
-              ]}
-            >
-              <List.Item.Meta title={`${movie.Title} (${movie.Year})`} />
-            </List.Item>
-          )}
-        />
-        <Button
-          color={"blue"}
-          onClick={this.props.onSubmit}
-          disabled={numMovies > 5 || numMovies === 0}
-          type="primary"
-        >
-          Submit nominations
-        </Button>
-        {numMovies > 5 && (
-          <p className={"error-msg"}>
-            You cannot select more than 5 movies to nominate
-          </p>
-        )}
-      </div>
-    );
-  }
+		return (
+			<div className={"shortlist-container"}>
+				<h2 style={{ width: "100%", textAlign: "center" }}>Your nominations</h2>
+				<p style={{ width: "100%", textAlign: "center", fontSize: "0.9em" }}>
+					(select up to 5 movies to nominate)
+				</p>
+				<List
+					itemLayout="horizontal"
+					bordered
+					dataSource={this.props.shortList}
+					renderItem={(movie: IMovie) => (
+						<List.Item
+							actions={[
+								<DeleteOutlined
+									color={"#eb2f96"}
+									onClick={() => this.props.onRemoveMovie(movie.imdbID)}
+								/>,
+							]}
+						>
+							<List.Item.Meta title={`${movie.Title} (${movie.Year})`} />
+						</List.Item>
+					)}
+				/>
+				<Button
+					color={"blue"}
+					onClick={this.props.onSubmit}
+					disabled={numMovies > 5 || numMovies === 0}
+					type="primary"
+				>
+					Submit nominations
+				</Button>
+				{numMovies > 5 && (
+					<p className={"error-msg"}>
+						You cannot select more than 5 movies to nominate
+					</p>
+				)}
+			</div>
+		);
+	}
 }
 
 export interface IShortlistComponentState {
-  // nothing
+	// nothing
 }
 
 export interface IShortlistComponentProps {
-  shortList: IMovie[]; //list of movies in the shortlist
-  onRemoveMovie: (movieId: string) => void; // when the user removes a movie from the shortlist
-  onSubmit: () => void; // when the user clicks the button to submit the nominations
+	shortList: IMovie[]; //list of movies in the shortlist
+	onRemoveMovie: (movieId: string) => void; // when the user removes a movie from the shortlist
+	onSubmit: () => void; // when the user clicks the button to submit the nominations
 }
